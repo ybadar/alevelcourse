fe7a6571d3746cef41f881986a74ed9c74c50387
Mashooq Badar <mashooq.badar@gmail.com>
2022-10-19 22:00:58 +0100

    hello

--- fileio/fileio/Program.cs
+++ fileio/fileio/Program.cs
@@ -6,7 +6,7 @@ namespace fileio
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! and universe");
+            Console.WriteLine("Hello World!");
         }
     }
 }
